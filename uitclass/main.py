1453c5df613985e1455c3e04a1d86e229da6523c
Asharib Ali <asharibarain4@gmail.com>
2024-03-23 16:41:35 +0500

    update

--- uitclass/main.py
+++ uitclass/main.py
@@ -5,15 +5,14 @@ from fastapi import FastAPI
 app = FastAPI(
     title="Hello World API",
     version="0.0.1",
-    servers=[
-        {
-            "url": "https://066c-119-73-102-23.ngrok-free.app/",  # ADD NGROK URL Here Before Creating GPT Action
-            "description": "Development Server",
-        }
-    ],
 )
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
